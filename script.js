31807900d75a10ddc5fe6e21d8dfe54220c4727d
Neeraj Kumar Shukla <132157056+Neerajshukla12@users.noreply.github.com>
2025-10-25 13:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function majorityElement(nums) {
+    const frequency = {};
+    const n = nums.length;
+    const majorityCount = Math.floor(n / 2);
+
+    for (let num of nums) {
+        if (!frequency[num]) {
+            frequency[num] = 1;
+        } else {
+            frequency[num]++;
+        }
+
+        if (frequency[num] > majorityCount) {
+            return num;
+        }
+    }
+}
